4bbc4de8bd6eb4836160477924123a19c0e7ef15
James Brown <jbrown@svix.com>
2026-04-23 16:45:08 -0700

    remove unused tempdir

--- script.sh
+++ script.sh
@@ -7,13 +7,6 @@ fi
 
 set -euo pipefail
 
-wd="$(mktemp -d)"
-
-on_exit() {
-    rm -rf "$wd"
-}
-trap on_exit EXIT INT TERM
-
 for pv in $(kubectl -n "$NAMESPACE" get pv -o custom-columns=NAME:.metadata.name,CLAIM:.spec.claimRef.name | grep '\(clickhouse-data\)\|keeper' | cut -d' ' -f 1); do
     reclaim_policy="$(kubectl -n "$NAMESPACE" get pv "$pv" -o 'go-template={{.spec.persistentVolumeReclaimPolicy}}')"
     if [[ "$reclaim_policy" != "Retain" ]]; then
